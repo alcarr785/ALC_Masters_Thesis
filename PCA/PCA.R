a32f11c5d8a32d10e24b416ffcbcf9aadc7f8eab
alcarr <alcarr@uri.edu>
2023-10-16 14:51:17 -0400

    Add PCA folder

---
+++ PCA/PCA.R
@@ -0,0 +1,20 @@
+#PCA Analysis
+library(tidyverse)
+pca <- read_table2("RISG_f.eigenvec", col_names = FALSE)
+eigenval <- scan("RISG_f.eigenval")
+
+# set names
+names(pca)[1] <- "Population"
+names(pca)[2] <- "Indv"
+names(pca)[3:ncol(pca)] <- paste0("PC", 1:(ncol(pca)-1))
+
+
+pca <- as.tibble(data.frame(pca, Population))
+pve <- data.frame(PC = 1:20, pve = eigenval/sum(eigenval)*100)
+a <- ggplot(pve, aes(PC, pve)) + geom_bar(stat = "identity")
+a + ylab("Percentage variance explained") + theme_light()
+
+b <- ggplot(pca, aes(PC1, PC2, col = Population)) + geom_point(size = 3)
+b <- b + coord_equal() + theme_light()
+b + xlab(paste0("PC1 (", signif(pve$pve[1], 3), "%)")) + ylab(paste0("PC2 (", signif(pve$pve[2], 3), "%)"))
+plot(b)
