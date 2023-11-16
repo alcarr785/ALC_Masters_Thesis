02bc5014bc4e5dc83e9913509c118f7fa26132a8
alcarr <alcarr@uri.edu>
2023-11-16 10:44:50 -0500

    Redid HWE

--- GeneticDiversity/HWE/HWE.sh
+++ GeneticDiversity/HWE/HWE.sh
@@ -3,6 +3,6 @@ for pop in "NEH" "FI" "CT" "GH" "NR" "MV";
 do
 	total=$(tail -n +2 "$pop.hwe" | wc -l)
 	HWE_sig=$(awk 'NR > 1 && $9<0.05 {count++} END {print count}' "$pop.hwe")
-	percentage=$(echo "scale=3; $HWE_sig / $total" | bc)
+	percentage=$(echo "scale=4; $HWE_sig / $total" | bc)
 	echo "$pop	$percentage" >>HWE.txt
 done
